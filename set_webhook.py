cc7c9904126da7f7744c24b69497d5e07d232c19
M.R.co <ghasemi.foad16@gmail.com>
2025-04-06 11:12:55 +0330

    Remove a confidential file

--- set_webhook.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-BOT_TOKEN = "7789974186:AAFOmZQx8upOgP-JgoHOTn8Cj2eH0E7fhxk"
-WEBHOOK_URL = "https://telegram-music-bot-664h.onrender.com/webhook"
-
-response = requests.get(f"https://api.telegram.org/bot{BOT_TOKEN}/setWebhook?url={WEBHOOK_URL}")
-
-print(response.json())  # Check the response
-#python set_webhook.py
